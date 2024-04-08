747bc4b47d4fd21f31e95d6abfd328eb8748beb3
gitKraken <gitkraken@mail.id>
2024-04-08 05:22:42 -0400

    05 How to configure Spring Security Authorization

--- springbootsecurity/src/main/java/io/javabrains/springbootsecurity/HomeResource.java
+++ springbootsecurity/src/main/java/io/javabrains/springbootsecurity/HomeResource.java
@@ -9,4 +9,12 @@ public class HomeResource {
     public String home(){
         return "<h1>Welcome</h1>";
     }
+    @RequestMapping(path = "/user", method = RequestMethod.GET)
+    public String user(){
+        return "<h1>Welcome User</h1>";
+    }
+    @RequestMapping(path = "/admin", method = RequestMethod.GET)
+    public String admin(){
+        return "<h1>Welcome Admin</h1>";
+    }
 }
--- springbootsecurity/src/main/java/io/javabrains/springbootsecurity/SecurityConfiguration.java
+++ springbootsecurity/src/main/java/io/javabrains/springbootsecurity/SecurityConfiguration.java
@@ -1,10 +1,12 @@
 package io.javabrains.springbootsecurity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -20,8 +22,17 @@ public class SecurityConfiguration {
             .username("foo")
             .password("foo")
             .roles("ADMIN")
-            .build();
-            
+            .build();            
         return new InMemoryUserDetailsManager(account1,account2);
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+            .requestMatchers("/admin").hasRole("ADMIN")
+            .requestMatchers("/user").hasAnyRole("USER", "ADMIN")
+            .requestMatchers("/").permitAll()            
+            .and()
+            .formLogin();                        
+        return http.build();
     }
 }
